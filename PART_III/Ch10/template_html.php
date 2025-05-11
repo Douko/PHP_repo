ef3cd9fcdf82c20bd05220bb5c2f8f33967a86d1
Douko <bryanpq3237@gmail.com>
2025-05-11 14:51:06 -0500

    changes

--- PART_III/Ch10/template_html.php
+++ PART_III/Ch10/template_html.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>Home</title>
-</head>
-<body>
-<p>Welcome to My Great Website<br />
-today is
-<?php
-    print date('F d, Y');
-?>
-</p>
-</body>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Home</title>
+    </head>
+    <body>
+        <p>Welcome to My Great Website<br />
+            today is</p>
+        <?php
+            echo date('F d, Y');
+        ?>
+        
+    </body>
 </html>
